d88f72fb2e05b664ecbec5d1e5507c14f9981cf5
Nguyen Minh Hien <nmhienbn@gmail.com>
2023-05-07 09:32:51 +0700

    test game

--- src/events/out_game/choose_level/choose_level.hpp
+++ src/events/out_game/choose_level/choose_level.hpp
@@ -2,7 +2,7 @@
 #include "draw/render_elements.hpp"
 #include "player/player_data.hpp"
 #include "level/level_data.hpp"
-#include "click_handle/out_game/quit_menu/quit_menu.hpp"
-#include "click_handle/out_game/reset_level/reset_menu.hpp"
+#include "events/out_game/quit_menu/quit_menu.hpp"
+#include "events/out_game/reset_level/reset_menu.hpp"
 
 void handle_choosing_level_screen();
--- src/game.hpp
+++ src/game.hpp
@@ -4,11 +4,11 @@
 #include "elements/elements_actions.hpp"
 #include "music/music.hpp"
 
-#include "click_handle/out_game/start_game/start_game.hpp"
-#include "click_handle/out_game/choose_level/choose_level.hpp"
-#include "click_handle/out_game/unlock_plant/unlock_plant.hpp"
+#include "events/out_game/start_game/start_game.hpp"
+#include "events/out_game/choose_level/choose_level.hpp"
+#include "events/out_game/unlock_plant/unlock_plant.hpp"
 
-#include "click_handle/in_game/game/player_click.hpp"
-#include "click_handle/in_game/game/level.hpp"
+#include "events/in_game/game/player_click.hpp"
+#include "events/in_game/game/level.hpp"
 
-#include "click_handle/mouse_cursor/mouse_cursor.hpp"
+#include "events/mouse_cursor/mouse_cursor.hpp"
